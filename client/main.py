d049ea659dce8454f79583d8e5edd3ea8e7791bd
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2023-05-13 18:41:25 +0200

    Update fipy for two-way comunication

--- client/main.py
+++ client/main.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI
 from network import WLAN
 import machine
 import urequests as request
@@ -5,9 +6,33 @@ import sensors
 import json
 import time
 
+app = FastAPI()
 wlan = WLAN(mode=WLAN.STA)
+sleep_time = 60
 
-print("Trying to connect...")
+@app.get("/read-sensors/")
+async def read_sensors():
+    list = [sensors.get_acceleration(), sensors.get_pressure(), sensors.get_temperature(),
+            sensors.get_humidity(), sensors.get_battery_percentage()]
+    post = dict()
+    for i in list:
+        post.update(i)
+
+    jsonPost = json.dumps(post)
+    print(jsonPost)
+
+    response = request.post("http://192.168.1.10:8000/sensors/", data=jsonPost) # 10.3.141.1
+    print(response.text)
+
+@app.post("/set-schedule/")
+async def set_schedule(interval : int):
+    global sleep_time
+    sleep_time = interval
+
+    return {"message": "Schedule set to " + str(interval) + " seconds"}
+
+print("Trying to connect...   ")
+# wlan.connect(ssid='ap_rasp', auth=(WLAN.WPA2, 'password'))
 wlan.connect(ssid='Vodafone-A80383340', auth=(WLAN.WPA2, 'HXHfMXLK2dqPMgHL'))
 
 while not wlan.isconnected():
@@ -17,16 +42,6 @@ print("WiFi connected succesfully")
 print(wlan.ifconfig())
 
 while True:
-    list = [sensors.get_acceleration(), sensors.get_pressure(), sensors.get_temperature(),
-        sensors.get_humidity(), sensors.get_battery_percentage()]
-    post = dict()
-    for i in list:
-        post.update(i)
+    read_sensors()
 
-    jsonPost = json.dumps(post)
-    print(jsonPost)
-
-    response = request.post("http://192.168.1.10:8000/sensors/", data=jsonPost)
-    print(response.text)
-
-    time.sleep(60)
+    time.sleep(sleep_time)
